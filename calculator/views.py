7b49c0b744446a7508547bff1e71f7bc45a871b4
Murat <murat.gaytemirov@hotmail.com>
2024-12-11 22:53:40 +0100

    added logic view

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    result = calculate(request)
+    return render(request, "index.html", {"result", result})
+
+
+def calculate(request):
+    result = 0
+    num1 = float(request.POST.get("num1"))
+    num2 = float(request.POST.get("num2"))
+    operator = request.POST.get("operator")
+    if request.method == "POST":
+        if operator == "+":
+            result = num1 + num2
+        elif operator == "-":
+            result = num1 - num2
+        elif operator == "*":
+            result = num1 * num2
+        elif operator == "/":
+            try:
+                result = num1 / num2
+            except ZeroDivisionError:
+                result = "Error: Division by zero"
+
+    return result
